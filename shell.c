ddfa40108b805f070db5fe0176c6a180e6a487d6
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-12-05 16:47:50 +0000

    create shell.c file & add libraries

---
+++ shell.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "shell.h"
+
+int main(void)
+{
+
