a545d72d220b55bae2d47cd386c48b83a95b562b
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-12-05 17:09:10 +0000

    switch around libraries from shell.c to shell.h

--- shell.c
+++ shell.c
@@ -1,10 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include "shell.h"
 
 int main(void)
--- shell.h
+++ shell.h
@@ -1,4 +1,17 @@
 #ifndef HEADER
 #define HEADER
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void prompt(); // Function to display the shell prompt
+char *read_command(); // Function to read user input
+void execute_command(char *command); // Function to execute the command
+void handle_errors(); // Function to handle errors
+
 #endif
