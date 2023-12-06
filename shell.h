e238fae53f46628bcf6b01d8c06271086eee0a1c
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-12-06 16:01:00 -0600

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,17 +1,48 @@
-#ifndef HEADER
-#define HEADER
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <stddef.h>
 
-void prompt(); // Function to display the shell prompt
-char *read_command(); // Function to read user input
-void execute_command(char *command); // Function to execute the command
-void handle_errors(); // Function to handle errors
+/* for read/write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
 
-#endif
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char **cmd_buf; /* pointer to cmd */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
+	int readfd;
+	int histcount;
+} info_t;
+
+/* getline */
+void _puts(const char *str);
+void _putchar(char c);
+void _puts(const char *str);
+void sigintHandler(int sig_num);
+
+#endif /* SHELL_H */
