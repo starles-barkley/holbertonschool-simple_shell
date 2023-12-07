7b710e4779c635cab69dc0c7f27ef784be6f56e2
colbertamir <colbertamir1516@gmail.com>
2023-12-07 21:10:10 +0000

    no strtok and exit is complete

--- exit.c
+++ exit.c
@@ -25,3 +25,40 @@ char *custom_strncpy(char *destination, char *source, int num_chars)
 	}
 	return (start);
 }
+
+/**
+ * Placeholder comment
+ */
+char *customStrncat(char *destination, char *source, int max_bytes)
+{
+	int dest_index = 0, src_index = 0;
+	char *result = destination;
+
+	while (destination[dest_index] != '\0')
+		dest_index++;
+
+	while (source[src_index] != '\0' && src_index < max_bytes)
+	{
+		destination[dest_index] = source[src_index];
+		dest_index++;
+		src_index++;
+	}
+
+	if (src_index < max_bytes)
+		destination[dest_index] = '\0';
+
+	return (result);
+}
+
+/**
+ * Placeholder comment
+ */
+char *customStrchr(char *str, char ch)
+{
+	do {
+		if (*str == ch)
+			return (str);
+	} while (*str++ != '\0');
+
+	return (NULL);
+}
