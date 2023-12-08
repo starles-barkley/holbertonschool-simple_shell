57429e80c6c02d80e5278bcd43ca53bb10dd16b0
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-12-08 04:35:49 +0000

    create exit for shell

--- shell.c
+++ shell.c
@@ -15,6 +15,12 @@ int main(int ac, char **argv){
       printf("%s", prompt);
       getline(&lineptr, &n, stdin);
       printf("%s\n", lineptr);
+
+      /* checks if the getline function failed or reached EOF or user use CTRL + D */ 
+
+        if (nchars_read == -1){
+            printf("Exiting shell\n");
+            return (-1);
     }
 
   free(lineptr);
