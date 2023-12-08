a0ed2739a7a5db57fe61f98a5867aa527d107b1c
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2023-12-08 04:20:14 +0000

    add pointers and void variables

--- shell.c
+++ shell.c
@@ -2,7 +2,15 @@
 
 int main(int ac, char **argv){
   char *prompt = "(Root) $ ";
+  char *lineptr;
+  size_t n = 0; 
+
+  (void)ac; (void)argv;
 
   printf("%s", prompt);
+  getline(&lineptr, &n, stdin);
+  printf("%s\n", lineptr);
+
+  free(lineptr);
   return (0);
 }
